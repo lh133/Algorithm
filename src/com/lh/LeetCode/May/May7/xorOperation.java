ec16943bb860aa57df22e49161ae9b37b95d6cbd
LH <1334130524@qq.com>
2021-05-07 10:00:42 +0800

    1486. 数组异或操作 增加数学方法对算式进行变换，寻找通式，将时间复杂度降到O(1)

--- src/com/lh/LeetCode/May/May7/xorOperation.java
+++ src/com/lh/LeetCode/May/May7/xorOperation.java
@@ -22,4 +22,24 @@ class Solution {
         }
         return ans;
     }
+
+    // 使用数学方法对算式进行变换
+    public int xorOperation2(int n, int start) {
+        int s = start >> 1, e = n & start & 1;
+        int ret = sumXor(s - 1) ^ sumXor(s + n - 1);
+        return ret << 1 | e;
+    }
+
+    private int sumXor(int x) {
+        if (x % 4 == 0) {
+            return x;
+        }
+        if (x % 4 == 1) {
+            return 1;
+        }
+        if (x % 4 == 2) {
+            return x + 1;
+        }
+        return 0;
+    }
 }
