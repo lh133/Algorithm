1f833736b76d0a1777282bcc5c53eabdf46612ba
LH <1334130524@qq.com>
2021-06-04 10:53:29 +0800

    160. 相交链表 双指针循环遍历两个链表

---
+++ src/com/lh/LeetCode/June/June4/ListNode.java
@@ -0,0 +1,17 @@
+package com.lh.LeetCode.June.June4;
+
+/**
+ * Definition for singly-linked list.
+ *
+ * @Author: LH
+ * @Date: 2021/6/4 9:56
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ src/com/lh/LeetCode/June/June4/getIntersectionNode.java
@@ -0,0 +1,35 @@
+package com.lh.LeetCode.June.June4;
+
+/**
+ * 160. 相交链表
+ * <p>
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ * <p>
+ * 提示：
+ * <p>
+ * listA 中节点数目为 m
+ * listB 中节点数目为 n
+ * 0 <= m, n <= 3 * 104
+ * 1 <= Node.val <= 105
+ * 0 <= skipA <= m
+ * 0 <= skipB <= n
+ * 如果 listA 和 listB 没有交点，intersectVal 为 0
+ * 如果 listA 和 listB 有交点，intersectVal == listA[skipA + 1] == listB[skipB + 1]
+ *
+ * @Author: LH
+ * @Date: 2021/6/4 9:55
+ */
+public class getIntersectionNode {
+}
+
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = (pA == null ? headB : pA.next);
+            pB = (pB == null ? headA : pB.next);
+        }
+        return pA;
+    }
+}
