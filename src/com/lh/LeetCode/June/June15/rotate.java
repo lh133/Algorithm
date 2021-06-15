1db25f6910d8aeb117fe10d247098245ed9f46b5
LH <1334130524@qq.com>
2021-06-15 17:16:56 +0800

    48. 旋转图像 将矩阵分为四块，使用临时变量保存一个值，依次替换值即可

---
+++ src/com/lh/LeetCode/June/June15/rotate.java
@@ -0,0 +1,35 @@
+package com.lh.LeetCode.June.June15;
+
+/**
+ * 48. 旋转图像
+ * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ * <p>
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ * <p>
+ * 提示：
+ * <p>
+ * matrix.length == n
+ * matrix[i].length == n
+ * 1 <= n <= 20
+ * -1000 <= matrix[i][j] <= 1000
+ *
+ * @Author: LH
+ * @Date: 2021/6/15 16:55
+ */
+public class rotate {
+}
+
+class Solution2 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n >> 1; i++) {
+            for (int j = 0; j < (n + 1) >> 1; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = tmp;
+            }
+        }
+    }
+}
