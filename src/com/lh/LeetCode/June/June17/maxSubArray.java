660adb414539d89fb2f75603e613e3c95632769c
LH <1334130524@qq.com>
2021-06-17 11:17:12 +0800

    53. 最大子序和 dp或分治法维护线段树解决

---
+++ src/com/lh/LeetCode/June/June17/maxSubArray.java
@@ -0,0 +1,73 @@
+package com.lh.LeetCode.June.June17;
+
+/**
+ * 53. 最大子序和
+ * <p>
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 3 * 104
+ * -105 <= nums[i] <= 105
+ * <p>
+ * 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。
+ *
+ * @Author: LH
+ * @Date: 2021/6/17 10:42
+ */
+public class maxSubArray {
+}
+
+class Solution3 {
+    // dp
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxSum = nums[0];
+        for (int num : nums) {
+            pre = Math.max(pre + num, num);
+            maxSum = Math.max(maxSum, pre);
+        }
+        return maxSum;
+    }
+}
+
+// 分治--线段树
+class Solution4 {
+    /**
+     * lSum 表示[l,r]内以l为左端点的最大子段和
+     * rSum 表示[l,r]内以r为右端点的最大子段和
+     * mSum 表示[l,r]内的最大子段和
+     * iSum 表示[l,r]的区间和
+     */
+    private class Status {
+        public int lSum, rSum, mSum, iSum;
+
+        public Status(int lSum, int rSum, int mSum, int iSum) {
+            this.lSum = lSum;
+            this.rSum = rSum;
+            this.mSum = mSum;
+            this.iSum = iSum;
+        }
+    }
+
+    private Status getInfo(int[] a, int l, int r) {
+        if (l == r) {
+            return new Status(a[l], a[l], a[l], a[l]);
+        }
+        int m = (l + r) >> 1;
+        Status lSub = getInfo(a, l, m);
+        Status rSub = getInfo(a, m + 1, r);
+        return pushUp(lSub, rSub);
+    }
+
+    private Status pushUp(Status l, Status r) {
+        int iSum = l.iSum + r.iSum;
+        int lSum = Math.max(l.lSum, l.iSum + r.lSum);
+        int rSum = Math.max(r.rSum, r.iSum + l.rSum);
+        int mSum = Math.max(Math.max(l.mSum, r.mSum), l.rSum + r.lSum);
+        return new Status(lSum, rSum, mSum, iSum);
+    }
+
+    public int maxSubArray(int[] nums) {
+        return getInfo(nums, 0, nums.length - 1).mSum;
+    }
+}
