84b850b5bff0e4767903c6261ef1805d10370310
LH <1334130524@qq.com>
2021-06-23 10:00:09 +0800

    66. 加一 该位数为9则进位，走完循环说明所有数都是9，则数组首位置1，扩容

---
+++ src/com/lh/LeetCode/June/June23/plusOne.java
@@ -0,0 +1,38 @@
+package com.lh.LeetCode.June.June23;
+
+/**
+ * 66. 加一
+ * <p>
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+ * <p>
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ * <p>
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= digits.length <= 100
+ * 0 <= digits[i] <= 9
+ *
+ * @Author: LH
+ * @Date: 2021/6/23 9:29
+ */
+public class plusOne {
+}
+
+class Solution2 {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        // 当所有位都是9时，数组扩容，首位变为一
+        int[] res = new int[n + 1];
+        res[0] = 1;
+        return res;
+    }
+}
