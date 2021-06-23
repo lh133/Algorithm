109f6c87753a42c0f4a6c263a75cf6ae8d38dc14
LH <1334130524@qq.com>
2021-06-23 09:43:21 +0800

    64. 最小路径和 动态规划

---
+++ src/com/lh/LeetCode/June/June22/minPathSum.java
@@ -0,0 +1,44 @@
+package com.lh.LeetCode.June.June22;
+
+/**
+ * 64. 最小路径和
+ * <p>
+ * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * <p>
+ * 说明：每次只能向下或者向右移动一步。
+ * <p>
+ * 提示：
+ * <p>
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 200
+ * 0 <= grid[i][j] <= 100
+ *
+ * @Author: LH
+ * @Date: 2021/6/22 10:11
+ */
+public class minPathSum {
+}
+
+class Solution2 {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int m = grid.length, n = grid[0].length;
+
+        // dp[i][j] 表示到达i，j位置的路径的最小数字总和
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
