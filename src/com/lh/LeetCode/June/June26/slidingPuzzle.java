7678dcd86146a9276786d545b169ce724101e27d
LH <1334130524@qq.com>
2021-06-26 22:10:51 +0800

    773. 滑动谜题 bfs，或A star搜索

---
+++ src/com/lh/LeetCode/June/June26/slidingPuzzle.java
@@ -0,0 +1,89 @@
+package com.lh.LeetCode.June.June26;
+
+import java.util.*;
+
+/**
+ * 773. 滑动谜题
+ * <p>
+ * 在一个 2 x 3 的板上（board）有 5 块砖瓦，用数字 1~5 来表示, 以及一块空缺用 0 来表示.
+ * <p>
+ * 一次移动定义为选择 0 与一个相邻的数字（上下左右）进行交换.
+ * <p>
+ * 最终当板 board 的结果是 [[1,2,3],[4,5,0]] 谜板被解开。
+ * <p>
+ * 给出一个谜板的初始状态，返回最少可以通过多少次移动解开谜板，如果不能解开谜板，则返回 -1 。
+ * <p>
+ * 提示：
+ * <p>
+ * board 是一个如上所述的 2 x 3 的数组.
+ * board[i][j] 是一个 [0, 1, 2, 3, 4, 5] 的排列.
+ *
+ * @Author: LH
+ * @Date: 2021/6/26 17:14
+ */
+public class slidingPuzzle {
+}
+
+class Solution {
+    // 预处理每个位置可以交换的值
+    int[][] neighbors = {{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}};
+
+    public int slidingPuzzle(int[][] board) {
+        // 初始状态转换为字符串
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                sb.append(board[i][j]);
+            }
+        }
+        String initial = sb.toString();
+        if ("123450".equals(initial)) {
+            return 0;
+        }
+
+        int step = 0;
+        // 保存所有状态
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(initial);
+        Set<String> seen = new HashSet<>();
+        seen.add(initial);
+
+        while (!queue.isEmpty()) {
+            ++step;
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                String status = queue.poll();
+                for (String nextStatus : get(status)) {
+                    if (!seen.contains(nextStatus)) {
+                        if ("123450".equals(nextStatus)) {
+                            return step;
+                        }
+                        queue.offer(nextStatus);
+                        seen.add(nextStatus);
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    // 枚举 status 通过一次交换操作得到的状态
+    public List<String> get(String status) {
+        List<String> ret = new ArrayList<String>();
+        char[] array = status.toCharArray();
+        int x = status.indexOf('0');
+        for (int y : neighbors[x]) {
+            swap(array, x, y);
+            ret.add(new String(array));
+            swap(array, x, y);
+        }
+        return ret;
+    }
+
+    public void swap(char[] array, int x, int y) {
+        char temp = array[x];
+        array[x] = array[y];
+        array[y] = temp;
+    }
+}
