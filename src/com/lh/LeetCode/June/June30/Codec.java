79157af2de3f7cccb77714603ea7ec51d8ce90db
LH <1334130524@qq.com>
2021-07-01 16:53:44 +0800

    剑指 Offer 37. 序列化二叉树 先序遍历

---
+++ src/com/lh/LeetCode/June/June30/Codec.java
@@ -0,0 +1,104 @@
+package com.lh.LeetCode.June.June30;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 剑指 Offer 37. 序列化二叉树
+ * <p>
+ * 请实现两个函数，分别用来序列化和反序列化二叉树。
+ * <p>
+ * 你需要设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+ * <p>
+ * 提示：输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+ *
+ * @Author: LH
+ * @Date: 2021/6/30 9:32
+ */
+public class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        return rSerialize(root, "");
+    }
+
+    // 先序遍历序列化
+    private String rSerialize(TreeNode root, String str) {
+        if (root == null) {
+            str += "None,";
+        } else {
+            str += root.val + ",";
+            str = rSerialize(root.left, str);
+            str = rSerialize(root.right, str);
+        }
+        return str;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] dataArr = data.split(",");
+        List<String> dataList = new LinkedList<>(Arrays.asList(dataArr));
+        return rDeserialize(dataList);
+    }
+
+    private TreeNode rDeserialize(List<String> dataList) {
+        if (dataList.get(0).equals("None")) {
+            dataList.remove(0);
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(dataList.get(0)));
+        dataList.remove(0);
+        root.left = rDeserialize(dataList);
+        root.right = rDeserialize(dataList);
+        return root;
+    }
+}
+
+// 括号表示编码 + 递归下降解码
+class codec2 {
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "X";
+        }
+        String left = "(" + serialize(root.left) + ")";
+        String right = "(" + serialize(root.right) + ")";
+        return left + root.val + right;
+    }
+
+    public TreeNode deserialize(String data) {
+        int[] ptr = {0};
+        return parse(data, ptr);
+    }
+
+    public TreeNode parse(String data, int[] ptr) {
+        if (data.charAt(ptr[0]) == 'X') {
+            ++ptr[0];
+            return null;
+        }
+        TreeNode cur = new TreeNode(0);
+        cur.left = parseSubtree(data, ptr);
+        cur.val = parseInt(data, ptr);
+        cur.right = parseSubtree(data, ptr);
+        return cur;
+    }
+
+    public TreeNode parseSubtree(String data, int[] ptr) {
+        ++ptr[0]; // 跳过左括号
+        TreeNode subtree = parse(data, ptr);
+        ++ptr[0]; // 跳过右括号
+        return subtree;
+    }
+
+    public int parseInt(String data, int[] ptr) {
+        int x = 0, sgn = 1;
+        if (!Character.isDigit(data.charAt(ptr[0]))) {
+            sgn = -1;
+            ++ptr[0];
+        }
+        while (Character.isDigit(data.charAt(ptr[0]))) {
+            x = x * 10 + data.charAt(ptr[0]++) - '0';
+        }
+        return x * sgn;
+    }
+}
