867d13112a17a78a80b000fc095f9c9cb502eb43
LH <1334130524@qq.com>
2021-07-01 16:59:13 +0800

    树节点定义

---
+++ src/com/lh/LeetCode/June/June28/TreeNode.java
@@ -0,0 +1,24 @@
+package com.lh.LeetCode.June.June28;
+
+/**
+ * @Author: LH
+ * @Date: 2021/6/28 11:04
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/com/lh/LeetCode/June/June30/TreeNode.java
@@ -0,0 +1,16 @@
+package com.lh.LeetCode.June.June30;
+
+/**
+ * @Author: LH
+ * @Date: 2021/6/30 9:33
+ */
+public class
+TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
