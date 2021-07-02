5b1fdeb1a0ea05ed7d18909a0d05fbd89035107b
LH <1334130524@qq.com>
2021-07-02 10:13:24 +0800

    1833. 雪糕的最大数量 计数排序+贪心

---
+++ src/com/lh/LeetCode/July/July2/maxIceCream.java
@@ -0,0 +1,46 @@
+package com.lh.LeetCode.July.July2;
+
+/**
+ * 1833. 雪糕的最大数量
+ * <p>
+ * 夏日炎炎，小男孩 Tony 想买一些雪糕消消暑。
+ * <p>
+ * 商店中新到 n 支雪糕，用长度为 n 的数组 costs 表示雪糕的定价，其中 costs[i] 表示第 i 支雪糕的现金价格。Tony 一共有 coins 现金可以用于消费，他想要买尽可能多的雪糕。
+ * <p>
+ * 给你价格数组 costs 和现金量 coins ，请你计算并返回 Tony 用 coins 现金能够买到的雪糕的 最大数量 。
+ * <p>
+ * 注意：Tony 可以按任意顺序购买雪糕。
+ * <p>
+ * 提示：
+ * <p>
+ * costs.length == n
+ * 1 <= n <= 105
+ * 1 <= costs[i] <= 105
+ * 1 <= coins <= 108
+ *
+ * @Author: LH
+ * @Date: 2021/7/2 10:04
+ */
+public class maxIceCream {
+}
+
+class Solution {
+    // 计数排序 + 贪心
+    public int maxIceCream(int[] costs, int coins) {
+        int[] freq = new int[100001];
+        for (int cost : costs) {
+            freq[cost]++;
+        }
+        int count = 0;
+        for (int i = 1; i <= 100000; i++) {
+            if (coins >= i) {
+                int curCount = Math.min(freq[i], coins / i);
+                count += curCount;
+                coins -= i * curCount;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+}
