b803f14a96d15cbb9e017f05c64b4992ca1567f0
LH <1334130524@qq.com>
2021-07-12 10:36:29 +0800

    981. 基于时间的键值存储 哈希表＋二分查找

---
+++ src/com/lh/LeetCode/July/July10/TimeMap.java
@@ -0,0 +1,105 @@
+package com.lh.LeetCode.July.July10;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 981. 基于时间的键值存储
+ * 创建一个基于时间的键值存储类 TimeMap，它支持下面两个操作：
+ * <p>
+ * 1. set(string key, string value, int timestamp)
+ * <p>
+ * 存储键 key、值 value，以及给定的时间戳 timestamp。
+ * 2. get(string key, int timestamp)
+ * <p>
+ * 返回先前调用 set(key, value, timestamp_prev) 所存储的值，其中 timestamp_prev <= timestamp。
+ * 如果有多个这样的值，则返回对应最大的  timestamp_prev 的那个值。
+ * 如果没有值，则返回空字符串（""）。
+ * <p>
+ * 提示：
+ * <p>
+ * 所有的键/值字符串都是小写的。
+ * 所有的键/值字符串长度都在 [1, 100] 范围内。
+ * 所有 TimeMap.set 操作中的时间戳 timestamps 都是严格递增的。
+ * 1 <= timestamp <= 10^7
+ * TimeMap.set 和 TimeMap.get 函数在每个测试用例中将（组合）调用总计 120000 次。
+ *
+ * @Author: LH
+ * @Date: 2021/7/12 10:26
+ */
+
+class Pair implements Comparable<Pair> {
+    int timestamp;
+    String value;
+
+    public Pair(int timestamp, String value) {
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+
+    public int hashCode() {
+        return timestamp + value.hashCode();
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Pair) {
+            Pair pair2 = (Pair) obj;
+            return this.timestamp == pair2.timestamp && this.value.equals(pair2.value);
+        }
+        return false;
+    }
+
+    public int compareTo(Pair pair2) {
+        if (this.timestamp != pair2.timestamp) {
+            return this.timestamp - pair2.timestamp;
+        } else {
+            return this.value.compareTo(pair2.value);
+        }
+    }
+}
+
+
+public class TimeMap {
+
+    Map<String, List<Pair>> map;
+
+    public TimeMap() {
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        List<Pair> pairs = map.getOrDefault(key, new ArrayList<Pair>());
+        pairs.add(new Pair(timestamp, value));
+        map.put(key, pairs);
+    }
+
+    public String get(String key, int timestamp) {
+        List<Pair> pairs = map.getOrDefault(key, new ArrayList<Pair>());
+        // 使用一个大于所有 value 的字符串，以确保在 pairs 中含有 timestamp 的情况下也返回大于 timestamp 的位置
+        Pair pair = new Pair(timestamp, String.valueOf((char) 127));
+        int i = binarySearch(pairs, pair);
+        if (i > 0) {
+            return pairs.get(i - 1).value;
+        }
+        return "";
+    }
+
+    private int binarySearch(List<Pair> pairs, Pair target) {
+        int low = 0, high = pairs.size() - 1;
+        if (high < 0 || pairs.get(high).compareTo(target) <= 0) {
+            return high + 1;
+        }
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            Pair pair = pairs.get(mid);
+            if (pair.compareTo(target) <= 0) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
