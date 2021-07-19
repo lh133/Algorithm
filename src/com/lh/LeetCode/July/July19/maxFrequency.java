e3fed74bbb66f08fb930cd00f192e41ae399a8c7
LH <1334130524@qq.com>
2021-07-19 10:18:17 +0800

    1838. 最高频元素的频数 滑动窗口

---
+++ src/com/lh/LeetCode/July/July19/maxFrequency.java
@@ -0,0 +1,42 @@
+package com.lh.LeetCode.July.July19;
+
+import java.util.Arrays;
+
+/**
+ * 1838. 最高频元素的频数
+ * <p>
+ * 元素的 频数 是该元素在一个数组中出现的次数。
+ * <p>
+ * 给你一个整数数组 nums 和一个整数 k 。在一步操作中，你可以选择 nums 的一个下标，并将该下标对应元素的值增加 1 。
+ * <p>
+ * 执行最多 k 次操作后，返回数组中最高频元素的 最大可能频数 。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 105
+ * 1 <= nums[i] <= 105
+ * 1 <= k <= 105
+ *
+ * @Author: LH
+ * @Date: 2021/7/19 10:02
+ */
+public class maxFrequency {
+}
+
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        long total = 0;
+        int l = 0, res = 1;
+        for (int r = 1; r < n; ++r) {
+            total += (nums[r] - nums[r - 1]) * (r - l);
+            while (total > k) {
+                total -= nums[r] - nums[l];
+                ++l;
+            }
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
