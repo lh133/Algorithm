25a573a29e928fbd11259377b6432085dc4f54da
LH <1334130524@qq.com>
2021-07-28 10:09:24 +0800

    1743. 从相邻元素对还原数组 哈希表还原，只出现一次的元素则为边界

---
+++ src/com/lh/LeetCode/July/July25/restoreArray.java
@@ -0,0 +1,62 @@
+package com.lh.LeetCode.July.July25;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 1743. 从相邻元素对还原数组
+ * <p>
+ * 存在一个由 n 个不同元素组成的整数数组 nums ，但你已经记不清具体内容。好在你还记得 nums 中的每一对相邻元素。
+ * <p>
+ * 给你一个二维整数数组 adjacentPairs ，大小为 n - 1 ，其中每个 adjacentPairs[i] = [ui, vi] 表示元素 ui 和 vi 在 nums 中相邻。
+ * <p>
+ * 题目数据保证所有由元素 nums[i] 和 nums[i+1] 组成的相邻元素对都存在于 adjacentPairs 中，存在形式可能是 [nums[i], nums[i+1]] ，也可能是 [nums[i+1], nums[i]] 。这些相邻元素对可以 按任意顺序 出现。
+ * <p>
+ * 返回 原始数组 nums 。如果存在多种解答，返回 其中任意一个 即可。
+ * <p>
+ * 提示：
+ * <p>
+ * nums.length == n
+ * adjacentPairs.length == n - 1
+ * adjacentPairs[i].length == 2
+ * 2 <= n <= 105
+ * -105 <= nums[i], ui, vi <= 105
+ * 题目数据保证存在一些以 adjacentPairs 作为元素对的数组 nums
+ *
+ * @Author: LH
+ * @Date: 2021/7/28 10:07
+ */
+public class restoreArray {
+}
+
+class Solution {
+    public int[] restoreArray(int[][] adjacentPairs) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int[] adjacentPair : adjacentPairs) {
+            map.putIfAbsent(adjacentPair[0], new ArrayList<>());
+            map.putIfAbsent(adjacentPair[1], new ArrayList<>());
+            map.get(adjacentPair[0]).add(adjacentPair[1]);
+            map.get(adjacentPair[1]).add(adjacentPair[0]);
+        }
+
+        int n = adjacentPairs.length + 1;
+        int[] ret = new int[n];
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            int e = entry.getKey();
+            List<Integer> adj = entry.getValue();
+            if (adj.size() == 1) {
+                ret[0] = e;
+                break;
+            }
+        }
+
+        ret[1] = map.get(ret[0]).get(0);
+        for (int i = 2; i < n; i++) {
+            List<Integer> adj = map.get(ret[i - 1]);
+            ret[i] = ret[i - 2] == adj.get(0) ? adj.get(1) : adj.get(0);
+        }
+        return ret;
+    }
+}
