db3c0c7dddb90ef7ed285954d618eff310e95918
LH <1334130524@qq.com>
2021-08-02 20:08:10 +0800

    1104. 二叉树寻路 数学方法找到规律，进行模拟即可

---
+++ src/com/lh/LeetCode/July/July29/pathInZigZagTree.java
@@ -0,0 +1,54 @@
+package com.lh.LeetCode.July.July29;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 1104. 二叉树寻路
+ * 在一棵无限的二叉树上，每个节点都有两个子节点，树中的节点 逐行 依次按 “之” 字形进行标记。
+ * <p>
+ * 如下图所示，在奇数行（即，第一行、第三行、第五行……）中，按从左到右的顺序进行标记；
+ * <p>
+ * 而偶数行（即，第二行、第四行、第六行……）中，按从右到左的顺序进行标记。
+ * <p>
+ * 给你树上某一个节点的标号 label，请你返回从根节点到该标号为 label 节点的路径，该路径是由途经的节点标号所组成的。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= label <= 10^6
+ *
+ * @Author: LH
+ * @Date: 2021/8/2 19:52
+ */
+public class pathInZigZagTree {
+}
+
+class Solution {
+    public List<Integer> pathInZigZagTree(int label) {
+        int row = 1, rowStart = 1;
+        while (rowStart * 2 <= label) {
+            row++;
+            rowStart *= 2;
+        }
+        if (row % 2 == 0) {
+            label = getReverse(label, row);
+        }
+        List<Integer> path = new ArrayList<>();
+        while (row > 0) {
+            if (row % 2 == 0) {
+                path.add(getReverse(label, row));
+            } else {
+                path.add(label);
+            }
+            row--;
+            label >>= 1;
+        }
+        Collections.reverse(path);
+        return path;
+    }
+
+    public int getReverse(int label, int row) {
+        return (1 << row - 1) + (1 << row) - 1 - label;
+    }
+}
