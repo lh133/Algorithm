07840ca00695817e40b63199b40dd689e633411c
LH <1334130524@qq.com>
2021-08-10 20:40:42 +0800

    1137. 第 N 个泰波那契数 动态规划

---
+++ src/com/lh/LeetCode/August/August8/tribonacci.java
@@ -0,0 +1,40 @@
+package com.lh.LeetCode.August.August8;
+
+/**
+ * 1137. 第 N 个泰波那契数
+ * <p>
+ * 泰波那契序列 Tn 定义如下：
+ * <p>
+ * T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
+ * <p>
+ * 给你整数 n，请返回第 n 个泰波那契数 Tn 的值。
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= n <= 37
+ * 答案保证是一个 32 位整数，即 answer <= 2^31 - 1。
+ *
+ * @Author: LH
+ * @Date: 2021/8/10 20:38
+ */
+public class tribonacci {
+}
+
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int p , q = 0, r = 1, s = 1;
+        for (int i = 3; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = s;
+            s = p + q + r;
+        }
+        return s;
+    }
+}
