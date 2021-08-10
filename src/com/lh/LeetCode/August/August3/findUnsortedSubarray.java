6274e0cee5fb56dc72e5373af24e571d0789c139
LH <1334130524@qq.com>
2021-08-10 16:53:23 +0800

    581. 最短无序连续子数组 将原数组分为三段，第一段和最后一段有序，从前往后遍历，最后一个不符合升序的为右边界，从后往前遍历，最后一个不符合升序的为左边界

---
+++ src/com/lh/LeetCode/August/August3/findUnsortedSubarray.java
@@ -0,0 +1,39 @@
+package com.lh.LeetCode.August.August3;
+
+/**
+ * 581. 最短无序连续子数组
+ * 给你一个整数数组 nums ，你需要找出一个 连续子数组 ，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+ * <p>
+ * 请你找出符合题意的 最短 子数组，并输出它的长度。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 104
+ * -105 <= nums[i] <= 105
+ *
+ * @Author: LH
+ * @Date: 2021/8/10 15:55
+ */
+public class findUnsortedSubarray {
+}
+
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int maxn = Integer.MIN_VALUE, right = -1;
+        int minn = Integer.MAX_VALUE, left = -1;
+        for (int i = 0; i < n; i++) {
+            if (maxn > nums[i]) {
+                right = i;
+            } else {
+                maxn = nums[i];
+            }
+            if (minn < nums[n - i - 1]) {
+                left = n - i - 1;
+            } else {
+                minn = nums[n - i - 1];
+            }
+        }
+        return right == -1 ? 0 : right - left + 1;
+    }
+}
