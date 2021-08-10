9f77ce9951ae2f51fec737e4213fa675c7ebded0
LH <1334130524@qq.com>
2021-08-10 20:43:12 +0800

    313. 超级丑数 最小堆

---
+++ src/com/lh/LeetCode/August/August9/nthSuperUglyNumber.java
@@ -0,0 +1,49 @@
+package com.lh.LeetCode.August.August9;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/**
+ * 313. 超级丑数
+ * <p>
+ * 超级丑数 是一个正整数，并满足其所有质因数都出现在质数数组 primes 中。
+ * <p>
+ * 给你一个整数 n 和一个整数数组 primes ，返回第 n 个 超级丑数 。
+ * <p>
+ * 题目数据保证第 n 个 超级丑数 在 32-bit 带符号整数范围内。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 106
+ * 1 <= primes.length <= 100
+ * 2 <= primes[i] <= 1000
+ * 题目数据 保证 primes[i] 是一个质数
+ * primes 中的所有值都 互不相同 ，且按 递增顺序 排列
+ *
+ * @Author: LH
+ * @Date: 2021/8/10 20:41
+ */
+public class nthSuperUglyNumber {
+}
+
+class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        Set<Long> seen = new HashSet<Long>();
+        PriorityQueue<Long> heap = new PriorityQueue<Long>();
+        seen.add(1L);
+        heap.offer(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int prime : primes) {
+                long next = curr * prime;
+                if (seen.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
+    }
+}
