f55fd869a7c30076c916281a2d3472eec3003893
LH <1334130524@qq.com>
2021-08-10 20:37:36 +0800

    457. 环形数组是否存在循环 快慢指针

---
+++ src/com/lh/LeetCode/August/August7/circularArrayLoop.java
@@ -0,0 +1,65 @@
+package com.lh.LeetCode.August.August7;
+
+/**
+ * 457. 环形数组是否存在循环
+ * <p>
+ * 存在一个不含 0 的 环形 数组 nums ，每个 nums[i] 都表示位于下标 i 的角色应该向前或向后移动的下标个数：
+ * <p>
+ * 如果 nums[i] 是正数，向前（下标递增方向）移动 |nums[i]| 步
+ * 如果 nums[i] 是负数，向后（下标递减方向）移动 |nums[i]| 步
+ * 因为数组是 环形 的，所以可以假设从最后一个元素向前移动一步会到达第一个元素，而第一个元素向后移动一步会到达最后一个元素。
+ * <p>
+ * 数组中的 循环 由长度为 k 的下标序列 seq 标识：
+ * <p>
+ * 遵循上述移动规则将导致一组重复下标序列 seq[0] -> seq[1] -> ... -> seq[k - 1] -> seq[0] -> ...
+ * 所有 nums[seq[j]] 应当不是 全正 就是 全负
+ * k > 1
+ * 如果 nums 中存在循环，返回 true ；否则，返回 false 。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 5000
+ * -1000 <= nums[i] <= 1000
+ * nums[i] != 0
+ *
+ * @Author: LH
+ * @Date: 2021/8/10 20:35
+ */
+public class circularArrayLoop {
+}
+
+class Solution{
+    public boolean circularArrayLoop(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                continue;
+            }
+            int slow = i, fast = next(nums, i);
+            // 判断非零且方向相同
+            while (nums[slow] * nums[fast] > 0 && nums[slow] * nums[next(nums, fast)] > 0) {
+                if (slow == fast) {
+                    if (slow != next(nums, slow)) {
+                        return true;
+                    } else {
+                        break;
+                    }
+                }
+                slow = next(nums, slow);
+                fast = next(nums, next(nums, fast));
+            }
+            int add = i;
+            while (nums[add] * nums[next(nums, add)] > 0) {
+                int tmp = add;
+                add = next(nums, add);
+                nums[tmp] = 0;
+            }
+        }
+        return false;
+    }
+
+    public int next(int[] nums, int cur) {
+        int n = nums.length;
+        return ((cur + nums[cur]) % n + n) % n; // 保证返回值在 [0,n) 中
+    }
+}
