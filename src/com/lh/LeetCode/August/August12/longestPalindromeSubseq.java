cc8ae3d5c1c1db55374766f240f45af599235cd6
LH <1334130524@qq.com>
2021-08-12 11:44:06 +0800

    516. 最长回文子序列 区间dp

---
+++ src/com/lh/LeetCode/August/August12/longestPalindromeSubseq.java
@@ -0,0 +1,40 @@
+package com.lh.LeetCode.August.August12;
+
+/**
+ * 516. 最长回文子序列
+ * <p>
+ * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+ * <p>
+ * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 1000
+ * s 仅由小写英文字母组成
+ *
+ * @Author: LH
+ * @Date: 2021/8/12 11:36
+ */
+public class longestPalindromeSubseq {
+}
+
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        // dp[i][j] 表示下标范围 [i,j] 内最长回文子序列的长度
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            char c1 = s.charAt(i);
+            for (int j = i + 1; j < n; j++) {
+                char c2 = s.charAt(j);
+                if (c1 == c2) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
