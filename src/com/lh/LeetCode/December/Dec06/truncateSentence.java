fa3b6bbb10bb8414980fc3345fa6acbd7848ab14
LH <1334130524@qq.com>
2021-12-06 14:44:35 +0800

    1816. 截断句子 循环即可

---
+++ src/com/lh/LeetCode/December/Dec06/truncateSentence.java
@@ -0,0 +1,39 @@
+package com.lh.LeetCode.December.Dec06;
+
+/**
+ * @Author: LH
+ * @Date: 2021/12/6 14:33
+ * <p>
+ * 1816. 截断句子
+ * <p>
+ * 句子 是一个单词列表，列表中的单词之间用单个空格隔开，且不存在前导或尾随空格。每个单词仅由大小写英文字母组成（不含标点符号）。
+ * 例如，"Hello World"、"HELLO" 和 "hello world hello world" 都是句子。
+ * 给你一个句子 s​​​​​​ 和一个整数 k​​​​​​ ，请你将 s​​ 截断 ​，​​​使截断后的句子仅含 前 k​​​​​​ 个单词。返回 截断 s​​​​​​ 后得到的句子。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 500
+ * k 的取值范围是 [1,  s 中单词的数目]
+ * s 仅由大小写英文字母和空格组成
+ * s 中的单词之间由单个空格隔开
+ * 不存在前导或尾随空格
+ */
+public class truncateSentence {
+}
+
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int n = s.length();
+        int end = 0, count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i == n || s.charAt(i) == ' ') {
+                count++;
+                if (count == k) {
+                    end = i;
+                    break;
+                }
+            }
+        }
+        return s.substring(0, end);
+    }
+}
